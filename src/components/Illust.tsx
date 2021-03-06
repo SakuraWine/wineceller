d18048303f39e7c0b19f87641a0dbe824f7ec0d0
Sakura Wine <sakura01n@gmail.com>
2021-03-06 16:32:36 +0900

    :four_leaf_clover: イラストのアスペクト比が崩れないように修正

--- src/components/Illust.tsx
+++ src/components/Illust.tsx
@@ -9,7 +9,7 @@ const Illust: FC<Props> = (props) => {
     const { title, url } = props;
     return (
         <div className="illust">
-            <img src={url} alt={title} width="300" height="300"></img>
+            <img src={url} alt={title} width="400"></img>
         </div>
     );
 };
