46ac2c73cc790c592926d8faa53ef144101fc1be
Sakura Wine <sakura01n@gmail.com>
2021-03-06 11:36:01 +0900

    :seedling: タイトルの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
 
 function App() {
   return (
-    <div className="App"></div>
+    <Title />
   );
 }
 
