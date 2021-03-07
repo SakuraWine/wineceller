f10e46c82e6d64782796937d4151c6598564fe28
Sakura Wine <sakura01n@gmail.com>
2021-03-07 12:23:16 +0900

    :seedling: css追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Title: FC = () => {
+    return (
+        <div className="header">
+            <h1>sakura-iro wineceller</h1>
+        </div>
+    );
+};
+
+export default Title;
